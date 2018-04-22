e06c0f28e1791abf04afd69a5fda8c65d64dea5a
Piotr Domanski <piotrdomanski@tlen.pl>
2018-04-22 11:56:44 +0200

    typy zmiennych

---
+++ js/5-datatypes.js
@@ -0,0 +1,45 @@
+'use strict'
+
+//typ liczbowy
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia
+
+console.log(calkowitaWyplata);
+
+
+
+//typ - lancuch znakow - string
+var wyplataStr = 5000;
+var premiaStr = 2500;
+var calkowitaWyplataStr;
+
+calkowitaWyplataStr = wyplataStr + premiaStr
+
+console.log(calkowitaWyplataStr);
+
+
+
+
+//typ logiczny - boolean
+
+var czyJestSmog;
+czyJestSmog = false;
+
+if (czyJestSmog) {
+	console.log("Jest SMOG");
+} else {
+	console.log("Nie ma SMOGa ;)");
+}
+
+
+
+//typ specjalny - undefied / null
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
