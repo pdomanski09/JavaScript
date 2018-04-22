5f60524a9cf9dec2748939488ebc170281b825cc
Piotr Domanski <piotrdomanski@tlen.pl>
2018-04-22 10:24:41 +0200

    stringi

---
+++ js/2-zmienne.js
@@ -0,0 +1,16 @@
+var imie;
+var imieDamskie = "Iza";
+var wiek = 37;
+
+var kolor = "czerwony";
+
+imie = "sergio"
+
+
+
+imie = imieDamskie
+
+console.log(imie);
+console.log(imieDamskie);
+console.log(wiek);
+console.log(kolor);
