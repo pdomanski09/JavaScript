219a1f6bd6c2c3255f04453eca7b3393935f69c3
Piotr Domanski <piotrdomanski@tlen.pl>
2018-04-22 16:52:26 +0200

    end

---
+++ js/13-warsztaty.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function multiply(dupa, dupa1, dupa3) {
+
+	var result =  dupa * dupa1 * dupa3;
+		
+	return result;
+}
+
+var wynikIloczynu = multiply(2,5,80);
+
+console.log(wynikIloczynu);
